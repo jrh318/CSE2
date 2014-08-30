7da05070a29085f9ae3d1fb16cdbd2da05385ff1
jrh318 <jrh318@lehigh.edu>
2014-08-30 15:34:11 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+/* Jordan Hess 
+Homework 01
+A better Hello World
+*/ 
+
+public class WelcomeClass{
+    
+    public static void main(String[] args){
+        
+        // say hello and such
+        
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println("^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-J--R--H--3--1--8-|");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("v  v  v  v  v  v");
+        System.out.println("I am afraid of the ocean but I try to not let that slow me down.");
+        
+        
+    }
+}
